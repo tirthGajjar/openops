a270ad1972408d416c26685c8cf5c404ef48cafa
Roman Snapko <roman@openops.com>
2025-06-04 16:03:09 +0300

    Fix flaky UI test (#742)
    Co-authored-by: Cezar <cezar@openops.com>

--- packages/ui-components/src/stories/ai-chat/ai-assistant-chat-container.stories.tsx
+++ packages/ui-components/src/stories/ai-chat/ai-assistant-chat-container.stories.tsx
@@ -144,6 +144,9 @@ export const WithMessages: Story = {
   args: {
     isEmpty: false,
   },
+  parameters: {
+    chromatic: { delay: 1000 },
+  },
   render: (args) => {
     const {
       aiChatSize,
@@ -174,6 +177,9 @@ export const WithMessages: Story = {
 export const WithMessagesCopyAndInject: Story = {
   args: {
     isEmpty: false,
+  },
+  parameters: {
+    chromatic: { delay: 500 },
   },
   render: (args) => {
     const {
