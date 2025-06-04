f72c499577fddce0ce25b303e110810299c333ab
Rita Gorokhod <60586879+rita-gorokhod@users.noreply.github.com>
2025-06-04 19:14:09 +0000

    Add custom api action

--- packages/blocks/flexera/src/index.ts
+++ packages/blocks/flexera/src/index.ts
@@ -1,3 +1,4 @@
+import { createCustomApiCallAction } from '@openops/blocks-common';
 import { createBlock } from '@openops/blocks-framework';
 import { flexeraAuth } from './auth';
 import { getClusterAggregatedSummaryCostsAction } from './lib/actions/get-cluster-aggregated-summary-costs-action';
@@ -9,6 +10,16 @@ export const flexera = createBlock({
   minimumSupportedRelease: '0.20.0',
   logoUrl: 'https://static.openops.com/blocks/flexera.png',
   authors: [],
-  actions: [getClustersAction, getClusterAggregatedSummaryCostsAction],
+  actions: [
+    getClustersAction,
+    getClusterAggregatedSummaryCostsAction,
+    createCustomApiCallAction({
+      baseUrl: () => 'https://api.spotinst.io',
+      auth: flexeraAuth,
+      authMapping: async (auth) => ({
+        Authorization: `Bearer ${(auth as any).apiToken}`,
+      }),
+    }),
+  ],
   triggers: [],
 });
