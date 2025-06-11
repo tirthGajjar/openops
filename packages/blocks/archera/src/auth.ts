9f157dba69ba99ec38aed6fe6d5d34f2e0741fd0
Rita Gorokhod <60586879+rita-gorokhod@users.noreply.github.com>
2025-06-11 09:03:02 +0200

    Add Archera connection instructions (#775)

--- packages/blocks/archera/src/auth.ts
+++ packages/blocks/archera/src/auth.ts
@@ -1,10 +1,25 @@
 import { BlockAuth, Property } from '@openops/blocks-framework';
 
+const markdown = `
+Connecting to the Archera API
+
+1. Go to [https://app.archera.ai](https://app.archera.ai) and log in to your account.
+
+2. Once logged in, go to the API section here: [https://app.archera.ai/settings?section=user&tab=api](https://app.archera.ai/settings?section=user&tab=api)
+
+3. Click Create New API Key if you do not already have one.
+
+4. Copy and securely store your API key.
+
+4. Find your Organization ID: your Organization ID (org_id) is displayed in the API section.
+
+5. Paste your API key and OrgId into the respective fields below`;
+
 export const archeraAuth = BlockAuth.CustomAuth({
   authProviderKey: 'Archera',
   authProviderDisplayName: 'Archera',
   authProviderLogoUrl: `https://static.openops.com/blocks/archera.jpeg`,
-  description: '',
+  description: markdown,
   required: true,
   props: {
     apiToken: Property.SecretText({
