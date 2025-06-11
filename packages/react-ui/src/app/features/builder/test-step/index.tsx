bd2a0dfbe1586a90127a0950f8045e3920d56592
Cezar <cezar@openops.com>
2025-06-11 15:32:04 +0300

    Add explanatory indicator with tooltip in TestStep component for sample output data (#772)

--- packages/react-ui/src/app/features/builder/test-step/index.tsx
+++ packages/react-ui/src/app/features/builder/test-step/index.tsx
@@ -7,9 +7,13 @@ import {
   TabsContent,
   TabsList,
   TabsTrigger,
+  Tooltip,
+  TooltipContent,
+  TooltipTrigger,
 } from '@openops/components/ui';
 import { ActionType, FlagId, TriggerType } from '@openops/shared';
 import { t } from 'i18next';
+import { Info } from 'lucide-react';
 import React from 'react';
 import { TestActionSection } from './test-action-section';
 import { TestTriggerSection } from './test-trigger-section';
@@ -50,9 +54,24 @@ const TestStepContainer = React.memo(
               </TabsTrigger>
               <TabsTrigger
                 value={TabListEnum.SAMPLE_STEP_OUTPUT}
-                className="font-bold text-primary-300 text-base pr-0 pl-2 dark:text-white rounded-none  border-b-2 data-[state=active]:bg-background data-[state=active]:text-primary-300 data-[state=active]:dark:text-white data-[state=active]:shadow-none data-[state=active]:border-blueAccent-300"
+                className="font-bold text-primary-300 text-base pr-0 pl-2 dark:text-white rounded-none border-b-2 data-[state=active]:bg-background data-[state=active]:text-primary-300 data-[state=active]:dark:text-white data-[state=active]:shadow-none data-[state=active]:border-blueAccent-300 gap-1"
               >
                 {t('Sample output data')}
+                <Tooltip>
+                  <TooltipTrigger asChild aria-label={t('Sample output info')}>
+                    <Info className="w-4 h-4 text-black" />
+                  </TooltipTrigger>
+                  <TooltipContent
+                    avoidCollisions
+                    hideWhenDetached
+                    side="bottom"
+                    className="font-medium text-left text-black max-w-[418px] text-wrap"
+                  >
+                    {t(
+                      'Sample data is for building and testing only - scheduled runs and test workflow are using the real step output.',
+                    )}
+                  </TooltipContent>
+                </Tooltip>
               </TabsTrigger>
             </TabsList>
             <TabsContent value={TabListEnum.STEP_OUTPUT}>
